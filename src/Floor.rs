cfac09440f6b0c0fdd2b9581d46adcd39d418c34
Nathaniel Richardson <ncrichardson15@gmail.com>
2023-07-25 00:27:18 -0400

    Initial Room Generation

--- src/Floor.rs
+++ src/Floor.rs
@@ -1,5 +1,7 @@
 use std::ops::Range;
 
+const MAX_TRIES: u32 = 1000;
+const PADDING: u32 = 5;
 
 pub struct Floor {
     rooms: Vec<Room>,
@@ -7,6 +9,7 @@ pub struct Floor {
     dimensions: Vec2,
 }
 
+#[derive(Copy, Clone)]
 pub struct Vec2 {
     x: u32,
     y: u32,
@@ -27,14 +30,61 @@ impl Floor {
         floor_dims: Vec2,
         room_count_range: Range<u32>,
         room_dims_range: Range<u32>
-    ) -> () {
+    ) -> Floor {
         let mut rooms = Vec::new();
         let room_count = fastrand::u32(room_count_range);
         for _ in 0..room_count {
             let room_dims = Vec2 {
-                x: fastrand::u32(room_dims_range),
-                y: fastrand::u32(room_dims_range),
+                x: fastrand::u32(room_dims_range.clone()),
+                y: fastrand::u32(room_dims_range.clone()),
             };
+            let mut placed = false;
+            let mut tries = 0;
+            while !placed {
+                let pos_loc = Vec2 {
+                    x: fastrand::u32(0..floor_dims.x - room_dims.x - PADDING),
+                    y: fastrand::u32(0..floor_dims.y - room_dims.y - PADDING)
+                };
+                let does_intersect = Floor::room_intersects_existing_rooms(pos_loc, room_dims, &rooms);
+                if !does_intersect {
+                    rooms.push(Room {
+                        dimensions: room_dims,
+                        rel_loc: pos_loc,
+                        connections: 0
+                    });
+                    placed = true;
+                }
+                tries += 1;
+
+                if tries == 1000 {
+                    break;
+                }
+            }
+        }
+        return Floor {
+            dimensions: floor_dims,
+            rooms: rooms,
+            hallways: Vec::new()
         }
     }
+
+    fn room_intersects_existing_rooms(
+        room_loc: Vec2,
+        room_dims: Vec2,
+        rooms: &Vec<Room>
+    ) -> bool {
+        rooms.iter().any(|other_room| {
+            (
+                room_loc.x >= other_room.rel_loc.x - 1
+                && room_loc.x <= other_room.rel_loc.x + other_room.dimensions.x + 1
+                || room_loc.x + room_dims.x >= other_room.rel_loc.x - 1
+                && room_loc.x + room_dims.x <= other_room.rel_loc.x + other_room.dimensions.x + 1
+            ) && (
+                room_loc.y >= other_room.rel_loc.y - 1
+                && room_loc.y <= other_room.rel_loc.y + other_room.dimensions.y + 1
+                || room_loc.y + room_dims.y >= other_room.rel_loc.y - 1
+                && room_loc.y + room_dims.y <= other_room.rel_loc.y + other_room.dimensions.y + 1)
+        })
+    }
+
 }
