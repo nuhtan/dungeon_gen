515c0db8e10672c6bfccfc0a0dd7907f531ab671
Nathaniel Richardson <ncrichardson15@gmail.com>
2023-07-29 01:57:57 -0400

    First try of hallway generation

--- src/dungeon.rs
+++ src/dungeon.rs
@@ -1,4 +1,4 @@
-use std::{ops::Range};
+use std::ops::Range;
 
 /// The maximum number of tries when attempting to place a new room into a floor.
 const MAX_TRIES: u32 = 1000;
@@ -8,7 +8,7 @@ const PADDING: u32 = 5;
 
 pub struct Floor {
     pub rooms: Option<Vec<Room>>,
-    _hallways: Option<Vec<Hallway>>,
+    pub hallways: Option<Vec<Hallway>>,
     pub connections: Option<Vec<Connection>>,
     dimensions: Vec2,
 }
@@ -33,6 +33,7 @@ pub struct Connection {
     pub ending_room: u32
 }
 
+#[derive(PartialEq)]
 enum Direction {
     Up,
     Down,
@@ -46,7 +47,7 @@ struct Segment {
     direction: Direction
 }
 
-struct Hallway {
+pub struct Hallway {
     starting_point: Vec2,
     ending_point: Vec2,
     segments: Vec<Segment>
@@ -64,7 +65,7 @@ impl Floor {
             dimensions: floor_dims,
             rooms: None,
             connections: None,
-            _hallways: None
+            hallways: None
         }
     }
 
@@ -141,12 +142,44 @@ impl Floor {
         }
     }
 
+    pub fn point_within_hallway(
+        &self,
+        location: Vec2
+    ) -> bool {
+        if let Some(hallways) = &self.hallways {
+            for hallway in hallways {
+                for segment in &hallway.segments {
+                    match segment.direction {
+                        Direction::Up | Direction::Down => {
+                            if location.x == segment.initial_location.x && (segment.initial_location.y..segment.initial_location.y + segment.length).contains(&location.y) {
+                                return true;
+                            }
+                        },
+                        _ => {
+                            if location.y == segment.initial_location.y && (segment.initial_location.x..segment.initial_location.x + segment.length).contains(&location.x) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        } else {
+            panic!("Hallways should be generated before attempting to render the floor")
+        }
+    }
+
     pub fn gen_connections(
         &mut self,
         extra_rooms_range: Range<u32>
     ) {
+        // Gets all of the rooms
         if let Some(rooms) = &self.rooms {
             let mut connections = Vec::new();
+            
+            // Create atleast one connection for each room except last.
+            // Because we create a default connection between each room in the list all rooms can
+            // be reached. Extra connections are attempted after the default connection is added.
             for room_num in 0..(rooms.len() - 1) {
                 let current_room = &rooms[room_num];
                 let next_room = &rooms[room_num + 1];
@@ -165,35 +198,134 @@ impl Floor {
         }
     }
 
-    fn hallway_intersects_room(
+    // Determines if a potential hallway would interesect any rooms on the floor
+    fn segment_intersects_room(
         &self,
         starting_point: Vec2,
         target_point: Vec2,
     ) -> Option<Vec2> {
-        unimplemented!("None or point that should be used for additional segment")
+        if let Some(rooms) = &self.rooms {
+            for room in rooms {
+                if starting_point.x == target_point.x
+                    && (room.rel_loc.x..room.rel_loc.x + room.dimensions.x).contains(&starting_point.x) 
+                    && (room.rel_loc.y <= starting_point.y.max(target_point.y)
+                        && target_point.y.min(starting_point.y) <= room.rel_loc.y + room.dimensions.y
+                    ) {
+                    if starting_point.y < target_point.y {
+                        return Some(Vec2::new(starting_point.x, room.rel_loc.y - 2));
+                    } else {
+                        return Some(Vec2::new(starting_point.y, room.rel_loc.y + room.dimensions.y + 2));
+                    }
+                } else if starting_point.y == target_point.y
+                    && (room.rel_loc.y..room.rel_loc.y + room.dimensions.y).contains(&starting_point.y)
+                    && (room.rel_loc.x <= starting_point.x.max(target_point.x)
+                        && target_point.x.min(starting_point.x) <= room.rel_loc.x + room.dimensions.x
+                    ) {
+                    if starting_point.x < target_point.x {
+                        return Some(Vec2::new(room.rel_loc.x - 2, starting_point.y));
+                    } else {
+                        return Some(Vec2::new(room.rel_loc.x + room.dimensions.x + 2, starting_point.y));
+                    }
+                }
+            }
+            return None;
+        } else {
+            panic!("Rooms must be generated before intersections can be checked")
+        }
     }
 
+    // Returns the world space location of a tile around a room that can be used to connect a
+    // hallway to the room.
     fn get_room_adjacent_tile(&self, room: &Room) -> Vec2 {
-        unimplemented!("Not yet")
+        let direction = match fastrand::u32(0..4) {
+            0 => Direction::Up,
+            1 => Direction::Down,
+            2 => Direction::Left,
+            _ => Direction::Right
+        };
+        return if [Direction::Up, Direction::Down].contains(&direction) {
+            Vec2::new(fastrand::u32(room.rel_loc.x..=room.rel_loc.x + room.dimensions.x), if direction == Direction::Down {
+                room.rel_loc.y - 1
+            } else {
+                room.rel_loc.y + room.dimensions.y + 1
+            })
+        } else {
+            Vec2::new(if direction == Direction::Left {
+                room.rel_loc.x - 1
+            } else {
+                room.rel_loc.x + room.dimensions.x + 1
+            }, fastrand::u32(room.rel_loc.y..=room.rel_loc.y + room.dimensions.y))
+        };
     }
 
     pub fn gen_hallways(
         &mut self
     ) {
+        // Get all of the rooms that have been generated
         if let Some(rooms) = &self.rooms {
+            // Get all of the intended connections
+            let mut hallways = Vec::new();
             if let Some(connections) = &self.connections {
                 for connection in connections {
+                    println!("Starting new connection");
                     let start_point = self.get_room_adjacent_tile(&rooms[connection.starting_room as usize]);
                     let end_point = self.get_room_adjacent_tile(&rooms[connection.ending_room as usize]);
-                    let current_point = start_point;
+                    let mut current_point = start_point;
+                    let mut segments = Vec::new();
+                    
+                    let mut last_dir = None;
+
                     while current_point != end_point {
-                        if i32::abs(current_point.x as i32 - end_point.x as i32) > i32::abs(current_point.y as i32 - end_point.y as i32) {
-                             
-                         } else {
-                            
+                        println!("{},{} -> {},{}", current_point.x, current_point.y, end_point.x, end_point.y);
+                        let mut target_point = if i32::abs(current_point.x as i32 - end_point.x as i32) > i32::abs(current_point.y as i32 - end_point.y as i32) || last_dir.map_or(false, |true_if_horizontal| true_if_horizontal == false) {
+                            Vec2::new(end_point.x, current_point.y)
+                        } else {
+                            Vec2::new(current_point.x, end_point.y)
+                        };
+                        
+                        println!("Initial Target Point: {}, {}", target_point.x, target_point.y);
+
+                        if let Some(early_terminating_point) = self.segment_intersects_room(current_point, target_point) {
+                           target_point = early_terminating_point; 
                         }
+
+                        println!("Updated Target Point: {}, {}", target_point.x, target_point.y);
+                        
+                        let l;
+                        let d;
+
+                        if current_point.x == target_point.x {
+                            last_dir = Some(false);
+                            l = i32::abs(current_point.y as i32 - target_point.y as i32);
+                            d = if current_point.y < target_point.y {
+                                    Direction::Up
+                                } else {
+                                    Direction::Down
+                                };
+                        } else {
+                            last_dir = Some(true);
+                            l = i32::abs(current_point.x as i32 - target_point.x as i32);
+                            d = if current_point.x < target_point.x {
+                                Direction::Right
+                            } else {
+                                Direction::Left
+                            };
+                        };
+
+                        segments.push(Segment {
+                            initial_location: current_point,
+                            length: l as u32,
+                            direction: d
+                        });
+                        current_point = target_point;
                     }
+                    hallways.push(Hallway {
+                        starting_point: start_point,
+                        ending_point: end_point,
+                        segments
+                    });
                 }
+                self.hallways = Some(hallways);
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,39 @@ use petgraph::{Undirected, Graph, dot::{Dot, Config}};
 fn main() {
     let floor_dims = Vec2::new(300, 50);
     let mut floor = Floor::init(floor_dims);
+
+    println!("Generating Rooms");
     floor.gen_rooms(10..20, 6..15);
     
-    for y in 0..floor_dims.y {
-        let mut line = "".to_string();
-        for x in 0..floor_dims.x {
-            line.push( match floor.point_within_room(Vec2::new(x, y)) {
-                true => ' ',
-                false => 'O'
-            });
+    // for y in 0..floor_dims.y {
+        // let mut line = "".to_string();
+        // for x in 0..floor_dims.x {
+            // line.push( match floor.point_within_room(Vec2::new(x, y)) {
+                // true => ' ',
+                // false => 'O'
+            // });
             
-        }
-        println!("{}", line);
-    }
+        // }
+        // println!("{}", line);
+    // }
 
+    println!("Generating Connections");
     floor.gen_connections(0..3);
+    
+    println!("Generating Hallways");
+    floor.gen_hallways();
 
-    let mut g = Graph::<String, (), Undirected>::new_undirected();
-    let mut graph_rooms = Vec::new();
-    if let Some(rooms) = &mut floor.rooms {
-        for room in rooms {
-            graph_rooms.push(g.add_node(room.index.to_string()))
-        }
-    }
-    if let Some(connections) = &mut floor.connections {
-        for connection in connections {
-            g.add_edge(graph_rooms[connection.starting_room as usize], graph_rooms[connection.ending_room as usize], ());
-        }
-    }
-    println!("{:?}", Dot::with_config(&g, &[Config::EdgeNoLabel]))
+    // let mut g = Graph::<String, (), Undirected>::new_undirected();
+    // let mut graph_rooms = Vec::new();
+    // if let Some(rooms) = &mut floor.rooms {
+        // for room in rooms {
+            // graph_rooms.push(g.add_node(room.index.to_string()))
+        // }
+    // }
+    // if let Some(connections) = &mut floor.connections {
+        // for connection in connections {
+            // g.add_edge(graph_rooms[connection.starting_room as usize], graph_rooms[connection.ending_room as usize], ());
+        // }
+    // }
+    // println!("{:?}", Dot::with_config(&g, &[Config::EdgeNoLabel]))
 }
